050ffc1d4c60ecb89e1821e357a515cad0697abd
Egor Liashuk <lyashuke007@gmail.com>
2025-02-05 13:10:44 +0200

    feat: comments added

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,10 @@
-const body = document.querySelector('body');
+const swiperSecond = new Swiper('.swiper_2', {
+	slidesPerView: 5,
+});
+
+const swiperFirst = new Swiper('.swiper_1', {
+	thumbs: {
+		swiper: swiperSecond,
+	},
+});
+
